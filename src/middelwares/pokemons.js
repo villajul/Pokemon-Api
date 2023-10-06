c69dbc87c19c6edd0dfee9eaa3d0536e9409aeac
Julio  Augusto Villacorta <101216193+villajul@users.noreply.github.com>
2023-10-06 18:01:09 -0300

    Update pokemons.js

--- src/middelwares/pokemons.js
+++ src/middelwares/pokemons.js
@@ -36,7 +36,7 @@ router.get("/", async (req, res) => {
       }
     } else {
       const result = await axios(
-        "https://pokeapi.co/api/v2/pokemon/?limit=100"
+        "https://pokeapi.co/api/v2/pokemon/?limit=50"
       )
         .then((r) => r.data)
         .then((r) => r.results);
